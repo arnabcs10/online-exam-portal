fecaece26a70f997faebe8420e2d5ffbf2a7ff73
arnabcs10 <arnab10panda@gmail.com>
2021-12-04 14:49:54 +0530

    Added home screen

--- frontend/src/app/views/Home/Home.js
+++ frontend/src/app/views/Home/Home.js
@@ -1,27 +1,53 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
 import useAuth from 'app/hooks/useAuth'
 import { Link } from 'react-router-dom'
-
-
+import TopBar from './TopBar'
+import useSettings from 'app/hooks/useSettings'
+import Footer from '../../components/Footer/Footer'
 const Home = () => {
+    const { settings, updateSettings } = useSettings()
+    const { layout1Settings, secondarySidebar } = settings
+    const topbarTheme = settings.themes[layout1Settings.topbar.theme]
     const {
         isAuthenticated,
         logout,
         // user
-    } = useAuth()
+    } = useAuth() 
+    const bgImgURL = '/assets/images/illustrations/home.jpg'
     return (
-        <div>
-            Home
+        <div
+            style={{
+                backgroundImage:`url(${bgImgURL})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'top',
+                backgroundSize: 'cover',
+                overflow: 'hidden',
+                top:0
+            }}
+        >
+            <ThemeProvider theme={topbarTheme}>
+                <TopBar fixed={true} className="elevation-z8" />
+            </ThemeProvider>
+            <div
+                style={{
+                    height:"85.6vh"
+                }}
+            >
+                Home
 
-            {
-                (isAuthenticated ? <div>
-                    <Link to='/class'>Go to Classroom</Link>
-                    <div style={{cursor:"pointer"}} onClick={logout}>SignOut</div>
-                    </div> 
-                :
-                <div><Link to='/session/signin'>SignIn</Link></div>
-                )
-            }
+                {
+                    (isAuthenticated ? <div>
+                        <Link to='/class'>Go to Classroom</Link>
+                        <div style={{cursor:"pointer"}} onClick={logout}>SignOut</div>
+                        </div> 
+                    :
+                    <div><Link to='/session/signin'>SignIn</Link></div>
+                    )
+                }
+            </div>
+            
+            <Footer/>
         </div>
     )
 }
